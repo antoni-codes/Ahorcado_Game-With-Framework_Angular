a5ff732419359efae6ea2b9c23a4ec3ca1943a80
Tony-sys <emmanuel_general@hotmail.com>
2021-02-09 12:02:35 -0600

    Logic, style, and structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
 
+//Decorador - typescript
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'ahorcado-projecto-final';
+
+  palabra = 'PASTO'; //property of the class
+  palabraOculta = '_ '; //property of the class
+
+
+  letras =  [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
+              'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 
+              'V', 'W', 'X', 'Y', 'Z'];
+
+  constructor(){
+    //Code for hide the word
+    //(this.) Reference to a property of the class
+    this.palabraOculta = '_ '.repeat( this.palabra.length );
+
+  }
+
+  //Method
+  comprobar( letra: any, ){
+                                  //Add a space o separation
+    const palabraOcultaArr = this.palabraOculta.split(' ');
+
+    for( let i = 0; i < this.palabra.length; i++){
+                
+      //[i]: Because each letter of the word will be traversed
+      if( this.palabra[i] === letra){
+        //Replace the value of the arrat
+        palabraOcultaArr[i] = letra;
+      }
+
+    }
+
+    this.palabraOculta = palabraOcultaArr.join(' ');
+
+
+  }
+
 }
